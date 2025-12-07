f699e650623a7ad15a17e1c7982cf3f581c0faa4
Andres Sanabria <andrelipe897@gmail.com>
2025-12-07 17:11:04 -0500

    Ajuste flake8

--- core/settings.py
+++ core/settings.py
@@ -106,7 +106,9 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": ("django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator"
+        ),
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
